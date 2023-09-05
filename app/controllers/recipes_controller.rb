42c32a29b98fa0a0ba08e8998feabd282c817138
Momo-87 <momochristian87@gmail.com>
2023-09-05 13:31:23 +0100

    Resolve n+1 problem in the recipe index action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @user = User.includes(:recipes).find_by(id: params[:user_id])
+    @recipes = @user.recipes.includes(:recipe_foods)
   end
 
   def show
