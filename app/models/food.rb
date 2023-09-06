114e7a04384f4dcaf1f50734b9dd1ce48e11a4e1
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-06 08:10:11 +0200

    Create food model

---
+++ app/models/food.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :description, presence: true
+
+  belongs_to :user, foreign_key: 'user_id'
+  has_many :recipe_foods, dependent: :destroy
+end
