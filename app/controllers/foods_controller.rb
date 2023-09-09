39eb715f166e8a4dd93831945b4cac6c7931ed8b
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-09 10:03:48 +0200

    Create shopping list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,7 +7,21 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
-  def shopping_list; end
+  def shopping_list
+    foods = Food.includes(:recipe_foods).where({ user_id: current_user.id })
+    @shopping_list = []
+    @total_price = 0
+    foods.each do |food|
+      shopping_list_item = {
+        food: food.name,
+        quantity: calculate_missing_food(food),
+        unit: food.measurement_unit,
+        unit_price: food.price
+      }
+      @total_price += shopping_list_item[:quantity] * shopping_list_item[:unit_price]
+      @shopping_list << shopping_list_item if (shopping_list_item[:quantity]).positive?
+    end
+  end
 
   def create
     name = food_params[:name]
@@ -36,5 +50,16 @@ class FoodsController < ApplicationController
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
+  def calculate_missing_food(food)
+    missing_food = -food.quantity
+    food.recipe_foods.each do |recipe_food|
+      missing_food += recipe_food.quantity
+    end
+
+    return 0 if missing_food <= 0
+
+    missing_food
   end
 end
