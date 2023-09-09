ba25eacef564717d0ec542234bff0d4ce2269bfd
Momo-87 <momochristian87@gmail.com>
2023-09-09 07:56:08 +0100

    Add navigation buttons

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,11 +7,10 @@ class Ability
     can :read, Recipe, public: true
 
     return unless user.persisted?
-
     can :manage, Recipe, user_id: user.id
+    can :manage, RecipeFood
 
     return unless user.role == 'admin'
-
     can :manage, :all
   end
 end
