49476d87163d48709c2fc531f7f8188a8b3e060b
Momo-87 <momochristian87@gmail.com>
2023-09-09 16:48:55 +0100

    Update tests and fix linter errors

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Food, type: :model do
   before :each do
-    @user = User.create!(name: 'Christian Momo', email: 'mm@recipapp.com', password: '123456') 
+    @user = User.create!(name: 'Christian Momo', email: 'mm@recipapp.com', password: '123456')
     @food = Food.create!(name: 'potato', measurement_unit: 'kg', price: 12, quantity: 3, user_id: @user.id)
   end
 
