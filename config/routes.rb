41d910a39b1e8f1b59c4579396e308dd48f7db2d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-09-09 04:02:20 +0200

    Create route route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
   get '/public_recipes', to: 'recipes#public_recipes'
 
   resources :foods, only: [:index, :new, :create]
