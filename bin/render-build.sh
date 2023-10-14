6f1a9edb2f1c36dc7956c54f5f49079a25045ad9
Momo-87 <momochristian87@gmail.com>
2023-10-14 10:34:11 +0100

    Update render-build.sh and create start.sh files for deployment on render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/web/.ruby-version)
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/web/Gemfile
+set_ruby_env $(fetch_or_build "$RUBY_VERSION")
+gem install bundler
+gem update --system
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
