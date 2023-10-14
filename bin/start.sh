08caf44fe26e47de0fb56cac43e2a474f8acdebe
Christian Romuald MOMO TONFACK <momochristian87@gmail.com>
2023-10-14 16:25:23 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/.ruby-version)
+export RUBY_VERSION=3.2.2
 export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/Gemfile
 source /home/render/ruby-env.sh
 set_ruby_env "$RUBIES_ROOT/ruby-$RUBY_VERSION"
