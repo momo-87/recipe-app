fee20947d8d3bfc1509681601a257b7e8743bcc1
Christian Romuald MOMO TONFACK <momochristian87@gmail.com>
2023-10-14 10:54:22 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/web/.ruby-version)
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/web/Gemfile
+export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/.ruby-version)
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/Gemfile
 source /home/render/ruby-env.sh
 set_ruby_env "$RUBIES_ROOT/ruby-$RUBY_VERSION"
 
-cd $RENDER_SRC_ROOT/web
+cd $RENDER_SRC_ROOT
 bundle exec puma -C ./config/puma.rb
