f4205338f85ddef5d3d284f45c94b95cc6c44e99
Christian Romuald MOMO TONFACK <momochristian87@gmail.com>
2023-10-14 10:52:56 +0100

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/web/.ruby-version)
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/web/Gemfile
+export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/.ruby-version)
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/Gemfile
 set_ruby_env $(fetch_or_build "$RUBY_VERSION")
 gem install bundler
 gem update --system
