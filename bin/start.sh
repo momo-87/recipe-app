286a192ac7ad5504497f43ee1086f422abd2b8ec
Christian Romuald MOMO TONFACK <momochristian87@gmail.com>
2023-10-15 14:59:32 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -6,6 +6,5 @@ export RUBY_VERSION=3.2.2
 export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/Gemfile
 source /home/render/ruby-env.sh
 set_ruby_env "$RUBIES_ROOT/ruby-$RUBY_VERSION"
-
 cd $RENDER_SRC_ROOT
 bundle exec puma -C ./config/puma.rb
